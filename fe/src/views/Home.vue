8f09557ec8027b7bdc3bcf61ea818dcd461a7bf2
egas <201502766@hufs.ac.kr>
2019-02-18 07:39:59 +0900

    axios

---
+++ fe/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+  <p>home</p>
+  <v-btn router :to ="'User'">test</v-btn>
+</div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
---
+++ fe/src/views/User.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="user">
+    <h1>user</h1>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    mounted () {
+      axios.get('http://localhost:3000/api/user')
+      .then((r) => {
+        console.log(r);
+      })
+      .catch((e) => {
+        console.error(e.message);
+      })
+    }
+  }
+</script>
