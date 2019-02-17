c2d55db255d1e013803f9c65b9aecb5879f32e43
egas <201502766@hufs.ac.kr>
2019-02-18 06:57:28 +0900

    web project start

---
+++ be/routes/api/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var createError = require('http-errors');
+var router = express.Router();
+
+router.get('/hello', function(req, res, next) {
+  res.send({msg: 'hello', a:1});
+});
+
+router.get('/user', function(req, res, next) {
+  res.send({msg: 'hello', a:1});
+});
+
+router.post('/user', function(req, res, next) {
+  res.send({msg: 'hello', a:1});
+});
+
+router.use('/test', require('./test'));
+router.use('/user', require('./user'));
+
+router.all('*', function(req, res, next) {
+  next(createError(404, '그런 api 없어요'));
+});
+
+module.exports = router;
---
+++ be/routes/api/user/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var createError = require('http-errors');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  const us = [
+    {
+      name: '김김김',
+      age: 14
+    },
+    {
+      name: '이이이',
+      age: 24
+    }
+  ]
+  res.send({users: us});
+});
+
+router.all('*', function(req, res, next) {
+  next(createError(404, '그런 api 없어요'));
+});
+
+module.exports = router;
