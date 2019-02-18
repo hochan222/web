1b1d6320204eb9fd06244a5d9a7628366a851a15
egas <201502766@hufs.ac.kr>
2019-02-18 21:19:12 +0900

    RESTful Data Test

--- be/app.js
+++ be/app.js
@@ -3,14 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const history = require('connect-history-api-fallback');
+const cors = require('cors');
 
 var app = express();
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use('/api',require('./routes/api'));
+app.use(history());
 
 app.use(express.static(path.join(__dirname, '../', 'fe', 'dist')));
 
